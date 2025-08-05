632d8cf15c4943a8b5d73d391a2376d2c7c24230
june276 <jungjune276@gmail.com>
2025-08-06 02:55:52 +0900

    메인 기본틀 완성

--- src/screens/MainScreen.jsx
+++ src/screens/MainScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const MainScreen = () => {
@@ -9,13 +9,29 @@ const MainScreen = () => {
 	return (
 		<View style={[styles.screenContainer, {paddingTop: insets.top,}]}>
 			<Text style={styles.mainText}>오늘의 식단을{'\n'}등록해주세요</Text>
+			<View style= {styles.cloudImgWrapper}>
+				<Image 
+					source={require('../assets/images/cloud.png')}
+					style={styles.cloudImg}
+				/>
+			</View>
+			<View style={styles.charImgWrapper}>
+				<Image
+					source={require('../assets/images/character.png')}
+					style={styles.characterImg}
+				/>
+			</View>
 		</View>
 	);
 };
 
+// TODO : Wrapper 2개를 묶고, screen을 space-between으로 설정,
+// 그러면 글자는 맨 위, 이미지 두개는 아래로 정렬,
+// 그 상태에서 이미지 크기 다시 조절해보기.
+
 const styles = StyleSheet.create({
 	screenContainer: {
-		// alignItems: "flex-start",
+		// flex: 3,
 		justifyContent: "flex-start",
 		width: "100%",
 		height: "100%",
@@ -29,6 +45,32 @@ const styles = StyleSheet.create({
 		paddingLeft: "40",
 		paddingTop: "50",
 	},
+	cloudImgWrapper: {
+		padding: 0,
+		margin: 0,
+		width: "100%",
+		height: "40%",
+		alignItems: "center",
+		justifyContent: "flex-end",
+	},
+	cloudImg: {
+		margin: 0,
+		width: "80%",
+		height: "60%",
+		resizeMode: "contain",
+	},
+	charImgWrapper: {
+		width: "100%",
+		height: "40%",
+		alignItems: "center",
+		justifyContent: 'flex-start',
+	},
+	characterImg: {
+		width: "80%",
+		height: "80%",
+		resizeMode: "contain",
+	},
+	
 });
 
 export default MainScreen;
