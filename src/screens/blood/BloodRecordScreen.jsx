126999450e2c51de865a6b016c340e9b3125cc15
june276 <jungjune276@gmail.com>
2025-08-12 15:09:44 +0900

    blood시작

--- src/screens/blood/BloodRecordScreen.jsx
+++ src/screens/blood/BloodRecordScreen.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const BloodRecordScreen = () => {
+	const insets = useSafeAreaInsets();
 	return (
-		<View>
+		<View style={styles.screenContainer}>
+			<View style={styles.topBar}>
+				<Text>M/D</Text>
+			</View>
 			<Text> 혈당 추가 </Text>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	screenContainer: {
+		width: "100%",
+		height: "100%",
+	},
+	topBar: {
+		width: "100%",
+		height: 50,
+		backgroundColor: "#fff",
+	},
+})
 
 export default BloodRecordScreen;
